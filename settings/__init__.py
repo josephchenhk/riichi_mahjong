b16c8f59b465956a61dfc63832fdd677f297859d
josephchen <josephchenhk@gmail.com>
2017-10-25 12:14:49 +0800

    ADD: setting abs path

---
+++ settings/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 11:53:51 2017
+
+@author: joseph.chen
+"""
+
---
+++ settings/settings.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 11:36:35 2017
+
+@author: joseph.chen
+"""
+
+abs_path = "C:\Users\joseph.chen\Projects\riichi_mahjong"
