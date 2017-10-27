347b8818e46c68918328f9d8e1a3cba064c7867a
josephchen <joseph.chen@digitcube.com>
2017-10-27 14:27:52 +0800

    CHG: fix abs data path

--- main_train_model.py
+++ main_train_model.py
@@ -9,6 +9,9 @@ import time
 from train_model.train_model import scores_data_preprocessing
 from train_model.train_model import waiting_tiles_data_preprocessing
 from train_model.train_model import train_scores_partial_fit
+from train_model.train_model import train_waiting_tiles_partial_fit
+from train_model.train_model import plot_scores
+from config.config import abs_data_path
 
 if __name__=="__main__":
 
@@ -16,7 +19,7 @@ if __name__=="__main__":
     
     
 #    is_waiting_data_preprocessing()
-    waiting_tiles_data_preprocessing() 
+#    waiting_tiles_data_preprocessing() 
 #    scores_data_preprocessing() 
     
 #    clf, avg_accuracy_scores, avg_auc_scores = train_is_waiting_partial_fit(load_classifier=False, save_classifier=True)
@@ -27,14 +30,14 @@ if __name__=="__main__":
 #                           "AUC_is_waiting")
 #                )
     
-#    for tile in range(34):
-#        clf, avg_accuracy_scores, avg_auc_scores = train_waiting_tiles_partial_fit(tile=tile, load_classifier=False, save_classifier=True)     
-#        plot_scores(avg_accuracy_scores, 
-#                    avg_auc_scores,
-#                    save_path="trained_classifiers/plots/",
-#                    save_name=("Accuracy_waiting_tile_{}.png".format(tile),
-#                               "AUC_waiting_tile_{}.png".format(tile))
-#                    )
+    for tile in range(34):
+        clf, avg_accuracy_scores, avg_auc_scores = train_waiting_tiles_partial_fit(tile=tile, load_classifier=False, save_classifier=True)     
+        plot_scores(avg_accuracy_scores, 
+                    avg_auc_scores,
+                    save_path=abs_data_path+"/train_model/trained_models/plots/",
+                    save_name=("Accuracy_waiting_tile_{}.png".format(tile),
+                               "AUC_waiting_tile_{}.png".format(tile))
+                    )
     
 #    clf, avg_mse_scores = train_scores_partial_fit(load_classifier=False, save_classifier=True)
 #    
